89c9da0022f4b98a3df8c0f495dda9327aba7b5c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 15:39:46 +0000

    Adding tarfile member sanitization to extractall()

--- tools/createBinaryArchives.py
+++ tools/createBinaryArchives.py
@@ -31,7 +31,29 @@ def createBinaryArchive(platform: str, arch: str) -> None:
     tmpDirPath = pathlib.Path(tmpDirPathStr)
 
     print("Extracting lsp-cli archive...")
-    with tarfile.open(lspCliArchivePath, "r:gz") as tarFile: tarFile.extractall(path=tmpDirPath)
+                                                             
+                                                             import os
+                                                             
+                                                             def is_within_directory(directory, target):
+                                                                 
+                                                                 abs_directory = os.path.abspath(directory)
+                                                                 abs_target = os.path.abspath(target)
+                                                             
+                                                                 prefix = os.path.commonprefix([abs_directory, abs_target])
+                                                                 
+                                                                 return prefix == abs_directory
+                                                             
+                                                             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                                                             
+                                                                 for member in tar.getmembers():
+                                                                     member_path = os.path.join(path, member.name)
+                                                                     if not is_within_directory(path, member_path):
+                                                                         raise Exception("Attempted Path Traversal in Tar File")
+                                                             
+                                                                 tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                                                 
+                                                             
+                                                             safe_extract(tarFile, path=tmpDirPath)
 
     lspCliDirPath = tmpDirPath.joinpath(f"lsp-cli-{lspCliVersion}")
     relativeJavaDirPath = downloadJava(tmpDirPath, lspCliDirPath, platform, arch)
@@ -86,7 +108,26 @@ def downloadJava(tmpDirPath: pathlib.Path, lspCliDirPath: pathlib.Path,
   if javaArchiveExtension == ".zip":
     with zipfile.ZipFile(javaArchivePath, "r") as zipFile: zipFile.extractall(path=tmpDirPath)
   else:
-    with tarfile.open(javaArchivePath, "r:gz") as tarFile: tarFile.extractall(path=tmpDirPath)
+                                                           def is_within_directory(directory, target):
+                                                               
+                                                               abs_directory = os.path.abspath(directory)
+                                                               abs_target = os.path.abspath(target)
+                                                           
+                                                               prefix = os.path.commonprefix([abs_directory, abs_target])
+                                                               
+                                                               return prefix == abs_directory
+                                                           
+                                                           def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                                                           
+                                                               for member in tar.getmembers():
+                                                                   member_path = os.path.join(path, member.name)
+                                                                   if not is_within_directory(path, member_path):
+                                                                       raise Exception("Attempted Path Traversal in Tar File")
+                                                           
+                                                               tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                                               
+                                                           
+                                                           safe_extract(tarFile, path=tmpDirPath)
 
   print("Removing JDK archive...")
   javaArchivePath.unlink()
